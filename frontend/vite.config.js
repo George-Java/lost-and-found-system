a0a2ca684974727baf5abb3434bba0392b45aeb4
linduoracle <linduoracle@gmail.com>
2026-07-17 20:16:46 +0800

    更新vite配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,23 +2,31 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      },
-      '/uploads': {
-        target: 'http://localhost:8080',
-        changeOrigin: true
-      },
-      '/ws': {
-        target: 'ws://localhost:8080',
-        ws: true,
-        changeOrigin: true
-      }
+    plugins: [vue()],
+    server: {
+        host: '0.0.0.0',  // 允许外部IP访问
+        port: 5173,
+        allowedHosts: [
+            '.serveousercontent.com',
+            'localhost',
+            '.trycloudflare.com',
+            'www.lostfound.icu',
+            'lostfound.icu'
+        ],
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true
+            },
+            '/uploads': {
+                target: 'http://localhost:8080',
+                changeOrigin: true
+            },
+            '/ws': {
+                target: 'ws://localhost:8080',
+                ws: true,
+                changeOrigin: true
+            }
+        }
     }
-  }
 })
